33eb8d14f4da89f615292e33a394b472157cc84d
Siddhartha Shakya <143714453+Sinmbf@users.noreply.github.com>
2026-08-24 21:03:33 +0545

    chore: configure PWA foundation

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,11 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(),VitePWA({registerType: "autoUpdate"})],
 })
+
+
